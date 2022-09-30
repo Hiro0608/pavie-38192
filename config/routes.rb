5deb99a13162368c15297b6d2cc09316d9ecfbad
Hiro0608 <h.satou1991@gmail.com>
2022-09-30 18:22:08 +0900

    userカラム・データベース編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'reservations/index'
-  root 'reservations#index'
+  devise_for :users
+  root to: 'reservations#index'
+  resources :reservations
+  #get 'reservations/index'
+  #root 'reservations#index'
   #ゲストログイン機能
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    passwords: 'users/passwords'
-  } 
-  devise_scope :user do
-    post '/users/guest_sign_in', to: 'users/sessions#new_guest'
-  end
+  #devise_for :users, controllers: {
+    #registrations: 'users/registrations',
+    #passwords: 'users/passwords'
+  #} 
+  #devise_scope :user do
+    #post '/users/guest_sign_in', to: 'users/sessions#new_guest'
+  #end
   #ゲストログイン機能
 end
