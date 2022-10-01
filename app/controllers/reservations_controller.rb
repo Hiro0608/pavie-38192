d8a1b61b94397c3e99f6e537c35b65277e7851c0
Hiro0608 <h.satou1991@gmail.com>
2022-10-01 18:39:48 +0900

    viewの修正

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,47 @@
 class ReservationsController < ApplicationController
+  before_action :set_reservation, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
+    @reservations = Reservation.all
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    Reservation.create(reservation_params)
+  end
+
+  def destroy
+    reservation = Reservation.find(params[:id])
+    reservation.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    reservation = Reservation.reservation(params[:id])
+    reservation.update(reservation_params)
+  end
+
+  def show
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:image, :title, :place, :introduction).merge(user_id: current_user.id)
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
