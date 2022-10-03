ade2a6b329c02d383c5fca2b74f6b61230974c12
Hiro0608 <h.satou1991@gmail.com>
2022-10-03 19:02:01 +0900

    投稿モデルのテストコード実施

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
-  factory :comment do
-    
+  factory :order_form do
+    post_code         { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
+    trading_area_id   { Faker::Number.between(from: 2, to: 48) }
+    city              { Faker::Address.city }
+    house_number      { Faker::Address.street_address }
+    phone_number      { Faker::Number.decimal_part(digits: 11) }
+    token             { Faker::Internet.password(min_length: 20, max_length: 30) }
   end
 end
