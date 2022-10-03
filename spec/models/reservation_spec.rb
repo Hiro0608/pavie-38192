29a7b85d59c952fd5b0305c78691336d105fccd4
Hiro0608 <h.satou1991@gmail.com>
2022-10-03 19:02:01 +0900

    投稿モデルのテストコード実施

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @reservation = FactoryBot.build(:reservation)
+  end
+  describe '新規投稿登録' do
+    context '投稿ができる場合' do
+      it "image,title,place,introductionが存在すれば登録できる" do
+        expect(@reservation).to be_valid
+      end
+    end  
+    context '投稿ができない場合' do
+      it "写真が添付されていないと登録できない" do
+        @reservation.image = nil
+        @reservation.valid?
+        expect(@reservation.errors.full_messages).to include("Imageを入力してください")
+      end
+      it "タイトルが入力されていなければ登録できない" do
+        @reservation.title = ''
+        @reservation.valid?
+        expect(@reservation.errors.full_messages).to include("Titleを入力してください")
+      end
+      it "場所が入力されていなければ登録できない" do
+        @reservation.place = ''
+        @reservation.valid?
+        expect(@reservation.errors.full_messages).to include("Placeを入力してください")
+      end
+      it "投稿の説明が入力されていなければ登録できない" do
+        @reservation.introduction = ''
+        @reservation.valid?
+        expect(@reservation.errors.full_messages).to include("Introductionを入力してください")
+      end
+      it "ユーザーが紐付いていない場合は登録できない" do
+        @reservation.user = nil
+        @reservation.valid?
+        expect(@reservation.errors.full_messages).to include("Userを入力してください")
+      end
+    end
+  end
 end
